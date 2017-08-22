a77a56af87af3a569be60b6cfe9e4cfa179aebb0
Prabin Kumar <prabin.kumar@gmail.com>
2017-08-22 16:15:12 +0530

    Assignment1.1

---
+++ MyProjects/src/Addition.java
@@ -0,0 +1,14 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a=10;
+		b=20;
+		c=a+b;
+		
+System.out.println("The value of c is: "  + c);
+	}
+
+}
